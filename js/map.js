01c69de9a80ec80d71a2052af99eeec360168572
Alexander <alexandergonzalezsaavedra@gmail.com>
2024-07-12 13:30:31 -0500

    ajustes solicitados cabios de color y espaciado

--- js/map.js
+++ js/map.js
@@ -1,15 +1,15 @@
-var map = L.map('map').setView([4.7110, -74.0721], 13);
+var map = L.map('map').setView([11.172680151718392, -74.23318947598895], 16);
 
 // Capa de mapa base de OpenStreetMap
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png',
     {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        maxZoom: 20,
+        attribution: '',
+        maxZoom: 40,
     }
 ).addTo(map);
 
 // Agrega un marcador en las coordenadas de Bogotá
-var marker = L.marker([4.7110, -74.0721]).addTo(map);
+var marker = L.marker([11.172680151718392, -74.23318947598895]).addTo(map);
 
 // Popup del marcador
-marker.bindPopup("<b>Hola Bogotá!</b><br>Esta es Bogotá.").openPopup();
+marker.bindPopup("<b>Playa Santúa</b><br>Calle 74 con carrera 2da con carrera 2da, Pozos Colorados, Santa Marta.").openPopup();

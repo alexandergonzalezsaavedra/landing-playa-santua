b96a23b686d4508037afd76a1392883a199f6ebd
Alexander <alexandergonzalezsaavedra@gmail.com>
2024-07-19 07:19:38 -0500

    ajuste icono volumen

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
-document.querySelector("#sonidoOceano").volume = .35; const estructura = document.querySelector("#activar-animacion-estructura"), menu = document.querySelector("#menu"), detalles = document.querySelector("#detalles"); let pathsEstructura = document.querySelectorAll(".animacionEstructura"), pathEstructuraUno = estructura.querySelector("#estructuraUno"), audio = document.querySelector("#sonidoOceano"), iconoSonido = document.querySelector("#silenciarMar i"), burgerToggle = document.querySelector("#burger-toggle"); const solenciarMar = () => { audio.classList.contains("silenciar") ? (audio.classList.remove("silenciar"), audio.play(), iconoSonido.classList.remove("fa-volume-low"), iconoSonido.classList.add("fa-volume-xmark")) : (audio.classList.add("silenciar"), audio.pause(), iconoSonido.classList.add("fa-volume-low"), iconoSonido.classList.remove("fa-volume-xmark")) }; document.addEventListener("click", (e => { e.target.matches(".enlaceMenu") && (burgerToggle.checked = !1) })), window.addEventListener("scroll", (() => { detalles.getBoundingClientRect().y <= 200 ? (menu.classList.add("modoOscuro"), menu.classList.remove("modoClaro")) : (menu.classList.remove("modoOscuro"), menu.classList.add("modoClaro")); const e = estructura.getBoundingClientRect().y; if (e < 450 || e <= 0) for (let e = 0; e < pathsEstructura.length; e++)pathsEstructura[e].classList.add("estructura"), pathsEstructura[e].classList.remove("ocultarEstructura"); else for (let e = 0; e < pathsEstructura.length; e++)pathsEstructura[e].classList.remove("estructura"), pathsEstructura[e].classList.add("ocultarEstructura") })), AOS.init();
-const formulario = document.querySelector("#formularioRegistro")
+document.querySelector("#sonidoOceano").volume = 0.35;
+const estructura = document.querySelector("#activar-animacion-estructura"),
+    menu = document.querySelector("#menu"),
+    detalles = document.querySelector("#detalles");
+let pathsEstructura = document.querySelectorAll(".animacionEstructura"),
+    pathEstructuraUno = estructura.querySelector("#estructuraUno"),
+    audio = document.querySelector("#sonidoOceano"),
+    iconoSonido = document.querySelector("#silenciarMar i"),
+    burgerToggle = document.querySelector("#burger-toggle");
+const solenciarMar = () => {
+    audio.classList.contains("silenciar")
+        ? (audio.classList.remove("silenciar"), audio.play(), iconoSonido.classList.remove("fa-volume-xmark"), iconoSonido.classList.add("fa-volume-low"))
+        : (audio.classList.add("silenciar"), audio.pause(), iconoSonido.classList.add("fa-volume-xmark"), iconoSonido.classList.remove("fa-volume-low"));
+};
+document.addEventListener("click", (e) => {
+    e.target.matches(".enlaceMenu") && (burgerToggle.checked = !1);
+}),
+    window.addEventListener("scroll", () => {
+        detalles.getBoundingClientRect().y <= 200 ? (menu.classList.add("modoOscuro"), menu.classList.remove("modoClaro")) : (menu.classList.remove("modoOscuro"), menu.classList.add("modoClaro"));
+        const e = estructura.getBoundingClientRect().y;
+        if (e < 450 || e <= 0) for (let e = 0; e < pathsEstructura.length; e++) pathsEstructura[e].classList.add("estructura"), pathsEstructura[e].classList.remove("ocultarEstructura");
+        else for (let e = 0; e < pathsEstructura.length; e++) pathsEstructura[e].classList.remove("estructura"), pathsEstructura[e].classList.add("ocultarEstructura");
+    }),
+    AOS.init();
+const formulario = document.querySelector("#formularioRegistro");
 let nombre = document.getElementById("nombre");
 let apellido = document.getElementById("apellido");
 let ciudad = document.getElementById("ciudad");
 let correo = document.getElementById("correo");
 let telefono = document.getElementById("telefono");
 let tyc = document.getElementById("terminos-condiciones");
-let mensajePreenvio = document.querySelector("#mensajePreenvio")
+let mensajePreenvio = document.querySelector("#mensajePreenvio");
 // Errores
-let errorNombre = document.getElementById("nombre-error")
-let errorApellido = document.getElementById("apellido-error")
-let errorCiudad = document.getElementById("ciudad-error")
-let errorCorreo = document.getElementById("correo-error")
-let errorTelefono = document.getElementById("telefono-error")
-let errorTyC = document.getElementById("checkbox-error")
+let errorNombre = document.getElementById("nombre-error");
+let errorApellido = document.getElementById("apellido-error");
+let errorCiudad = document.getElementById("ciudad-error");
+let errorCorreo = document.getElementById("correo-error");
+let errorTelefono = document.getElementById("telefono-error");
+let errorTyC = document.getElementById("checkbox-error");
 // Obtener utms
 let url = new URL(window.location.href);
 let queryParams = new URLSearchParams(url.search);
@@ -21,68 +44,68 @@ let source = queryParams.get("utm_source");
 let medium = queryParams.get("utm_medium");
 let campaign = queryParams.get("utm_campaign");
 let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-let isValid
+let isValid;
 formulario.addEventListener("submit", async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (nombre.value === "") {
-        errorNombre.classList.remove("d-none")
+        errorNombre.classList.remove("d-none");
         errorNombre.innerText = "¡El nombre es obligatorio!";
         isValid = false;
     }
     if (nombre.value !== "") {
         errorNombre.innerText = "";
-        errorNombre.classList.add("d-none")
+        errorNombre.classList.add("d-none");
     }
     if (apellido.value === "") {
-        errorApellido.classList.remove("d-none")
+        errorApellido.classList.remove("d-none");
         errorApellido.innerText = "¡El apellido es obligatorio!";
         isValid = false;
     }
     if (apellido.value !== "") {
-        errorApellido.classList.add("d-none")
+        errorApellido.classList.add("d-none");
         errorApellido.innerText = "";
     }
     if (ciudad.value === "") {
-        errorCiudad.classList.remove("d-none")
+        errorCiudad.classList.remove("d-none");
         errorCiudad.innerText = "¡La ciudad es obligatoria!";
         isValid = false;
     }
     if (ciudad.value !== "") {
-        errorCiudad.classList.add("d-none")
+        errorCiudad.classList.add("d-none");
         errorCiudad.innerText = "";
     }
     if (correo.value === "") {
-        errorCorreo.classList.remove("d-none")
+        errorCorreo.classList.remove("d-none");
         errorCorreo.innerText = "¡El coreo obligatorio!";
         isValid = false;
     }
     if (correo.value !== "") {
-        errorCorreo.classList.add("d-none")
+        errorCorreo.classList.add("d-none");
         errorCorreo.innerText = "";
     }
     if (telefono.value === "") {
-        errorTelefono.classList.remove("d-none")
+        errorTelefono.classList.remove("d-none");
         errorTelefono.innerText = "¡El teléfono obligatorio!";
         isValid = false;
     }
     if (telefono.value !== "") {
-        errorTelefono.classList.add("d-none")
+        errorTelefono.classList.add("d-none");
         errorTelefono.innerText = "";
     }
     if (nombre.value !== "" && apellido.value !== "" && ciudad.value !== "" && correo.value !== "" && telefono.value !== "" && tyc.checked) {
         isValid = true;
     }
     if (isValid) {
-        formulario.classList.add("d-none")
-        mensajePreenvio.classList.remove("d-none")
+        formulario.classList.add("d-none");
+        mensajePreenvio.classList.remove("d-none");
         try {
             setTimeout(() => {
-                location.replace(`./thank-you-page.html?name=${nombre.value}&tel=${telefono.value}&mail=${correo.value}`)
+                location.replace(`./thank-you-page.html?name=${nombre.value}&tel=${telefono.value}&mail=${correo.value}`);
             }, 6000);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         } finally {
-            mensajePreenvio.classList.remove("d-none")
+            mensajePreenvio.classList.remove("d-none");
         }
     }
-})
+});
